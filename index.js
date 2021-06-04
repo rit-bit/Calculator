dcb1951147330d3b3da96644d6cc4e1c2b429a20
Richard <71881973+rit-bit@users.noreply.github.com>
2021-06-04 12:53:17 +0100

    Refactored code for cleanliness, and made calculator keep running in a loop

--- index.js
+++ index.js
@@ -1,5 +1,46 @@
 const readline = require('readline-sync');
 
+printWelcomeMessage();
+while (true) {
+    const operator = chooseValidOperator();
+    const numberOfOperands = chooseNumberOfOperands(operator);
+    const operands = chooseOperands(numberOfOperands);
+    const answer = operate(operands, operator);
+    console.log(`The answer is: ${answer}\n`);
+}
+
+
+
+
+function printWelcomeMessage() {
+    //console.log('Welcome to the calculator!');
+    //console.log('==========================');
+    console.log(`
+     __    __     _                            _          _   _          
+    / / /\\ \\ \\___| | ___ ___  _ __ ___   ___  | |_ ___   | |_| |__   ___ 
+    \\ \\/  \\/ / _ \\ |/ __/ _ \\| '_ \` _ \\ / _ \\ | __/ _ \\  | __| '_ \\ / _ \\
+     \\  /\\  /  __/ | (_| (_) | | | | | |  __/ | || (_) | | |_| | | |  __/
+      \\/  \\/ \\___|_|\\___\\___/|_| |_| |_|\\___|  \\__\\___/   \\__|_| |_|\\___|`);
+    console.log(`
+     /$$$$$$            /$$                     /$$             /$$                                       /$$                     /$$
+    /$$__  $$          | $$                    | $$            | $$                                      |__/                    | $$
+   | $$  \\__/  /$$$$$$ | $$  /$$$$$$$ /$$   /$$| $$  /$$$$$$  /$$$$$$    /$$$$$$   /$$$$$$  /$$$$$$/$$$$  /$$ /$$$$$$$   /$$$$$$ | $$
+   | $$       |____  $$| $$ /$$_____/| $$  | $$| $$ |____  $$|_  $$_/   /$$__  $$ /$$__  $$| $$_  $$_  $$| $$| $$__  $$ |____  $$| $$
+   | $$        /$$$$$$$| $$| $$      | $$  | $$| $$  /$$$$$$$  | $$    | $$$$$$$$| $$  \\__/| $$ \\ $$ \\ $$| $$| $$  \\ $$  /$$$$$$$| $$
+   | $$    $$ /$$__  $$| $$| $$      | $$  | $$| $$ /$$__  $$  | $$ /$$| $$_____/| $$      | $$ | $$ | $$| $$| $$  | $$ /$$__  $$| $$
+   |  $$$$$$/|  $$$$$$$| $$|  $$$$$$$|  $$$$$$/| $$|  $$$$$$$  |  $$$$/|  $$$$$$$| $$      | $$ | $$ | $$| $$| $$  | $$|  $$$$$$$| $$
+    \\______/  \\_______/|__/ \\_______/ \\______/ |__/ \\_______/   \\___/   \\_______/|__/      |__/ |__/ |__/|__/|__/  |__/ \\_______/|__/`);
+}
+
+function chooseValidOperator() {
+    let operator, operatorValid;
+    ({operator, operatorValid} = getOperatorInput('\nPlease enter the operator:'));
+    while (!operatorValid) {
+        ({operator, operatorValid} = getOperatorInput('\nPlease enter a valid operator:'));
+    }
+    return operator;
+}
+
 function isOperatorValid(operator) {
     switch (operator) {
         case '*':
@@ -12,79 +53,65 @@ function isOperatorValid(operator) {
     }
 }
 
-function chooseOperator() {
-    console.log('Please enter the operator:');
+function getOperatorInput(message) {
+    console.log(message);
     let operator = readline.prompt();
     let operatorValid = isOperatorValid(operator);
-    while (!operatorValid) {
-        console.log('Please enter a valid operator:');
-        operator = readline.prompt();
-        operatorValid = isOperatorValid(operator);
+    console.log(operatorValid);
+    return ({operator, operatorValid});
+}
+
+function getNumberInput(message) {
+    console.log(message);
+    let input = readline.prompt();
+    return +input;
+}
+
+function chooseNumberOfOperands(operator) {
+    let numberOfOperands = "not a number";
+    while (isNaN(numberOfOperands) || numberOfOperands < 2) {
+        numberOfOperands = getNumberInput(`How many numbers do you want to ${operator}?`);
     }
-    return operator;
+    return numberOfOperands;
 }
 
-function chooseNumberOfArguments(operator) {
-    let numberOfArguments = "not a number";
-    while (isNaN(numberOfArguments) || numberOfArguments < 2) {
-        console.log(`How many numbers do you want to ${operator}?`);
-        let input = readline.prompt();
-        numberOfArguments = +input;
+function chooseOperands(numberOfOperands) {
+    const operandsArray = [];
+    for (let operandIx = 0; operandIx < numberOfOperands; operandIx++) {
+        operandsArray[operandIx] = chooseOperand(operandIx);
     }
-    return numberOfArguments;
+    return operandsArray;
 }
 
-function chooseArgument(argNumber) {
-    let argument = "not a number";
-    while (isNaN(argument)) {
-        console.log(`Please enter number ${argNumber + 1}:`);
-        let input = readline.prompt();
-        argument = +input;
+function chooseOperand(operandNum) {
+    let operand = "not a number";
+    while (isNaN(operand)) {
+        operand = getNumberInput(`Please enter number ${operandNum + 1}:`);
     }
-    return argument;
+    return operand;
 }
 
-console.log('Welcome to the calculator!');
-console.log('==========================');
+function operate(operands, operator) {
+    let answer = operands[0];
 
-const operator = chooseOperator();
-
-const numberOfArguments = chooseNumberOfArguments(operator);
-
-const arguments = [];
-for (let argNumber = 0; argNumber < numberOfArguments; argNumber++) {
-    arguments[argNumber] = chooseArgument(argNumber);
+    for (let ix = 1; ix < operands.length; ix++) {
+        switch (operator) {
+            case '*':
+                answer *= operands[ix];
+                break;
+            case '/':
+                answer /= operands[ix]; // TODO Handle errors e.g. 2 / 0 = ..?
+                break;
+            case '+':
+                answer += operands[ix];
+                break;
+            case '-':
+                answer -= operands[ix];
+                break;
+            default:
+                answer = '(invalid operator)'; // TODO This should be different
+                break;
+        }
+    }
+    return answer;
 }
-
-let answer;
-
-switch (operator) {
-    case '*':
-        answer = 1;
-        for (let arg of arguments) {
-            answer *= arg;
-        }
-        break;
-    case '/':
-        answer = arguments[0];
-        for (let argIx = 1; argIx < arguments.length; argIx++) {
-            answer /= arguments[argIx];
-        }
-        break;
-    case '+':
-        answer = 0;
-        for (let arg of arguments) {
-            answer += arg;
-        }
-        break;
-    case '-':
-        answer = arguments[0];
-        for (let argIx = 1; argIx < arguments.length; argIx++) {
-            answer -= arguments[argIx];
-        }
-        break;
-    default:
-        answer = '(invalid operator)';
-}
-
-console.log(`The answer is: ${answer}`);
